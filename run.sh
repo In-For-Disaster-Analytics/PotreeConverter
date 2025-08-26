55f0fc53c2f39ae7523bb88ff837bc02b22ad7a6
Maximiliano Osorio <maxiosorio@gmail.com>
2025-08-26 16:38:17 -0400

    fix: update the parent

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 OUTPUTS_DIR=${_tapisExecSystemOutputDir}
+WEB_SERVER_DIR=/corral/utexas/BCS24011/ckan/lidar_files
 
 potreeconverter -i ${converterInput} -o ${OUTPUTS_DIR}
-echo ${_tapisDtnSystemOutputDir}
-echo ${_tapisDtnSystemInputDir}
+
+PARENT_POINT_CLOUD_DIR=${WEB_SERVER_DIR}/${_tapisJobCreateDate}
+POINT_CLOUD_DIR=${PARENT_POINT_CLOUD_DIR}/${_tapisJobName}-${_tapisJobUuid}
+mkdir -p ${POINT_CLOUD_DIR}
+cp -r ${OUTPUTS_DIR} ${POINT_CLOUD_DIR}
+setfacl -R -m u:33:rx ${PARENT_POINT_CLOUD_DIR}
