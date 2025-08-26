813319bc18e0d13d9ebbb6909b2dd85749e64386
Maximiliano Osorio <maxiosorio@gmail.com>
2025-08-26 15:27:28 -0400

    add: app.json

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-
-INPUTS_DIR=${_tapisExecSystemInputDir}
 OUTPUTS_DIR=${_tapisExecSystemOutputDir}
 
-potreeconverter -i ${INPUTS_DIR}/ -o ${OUTPUTS_DIR}
-
-# Find the metadata.json on ${OUTPUTS_DIR} as stored as variable
-METADATA_FILE=$(find ${OUTPUTS_DIR} -name "metadata.json")
-
-python scripts/potree_scene_generator.py ${METADATA_FILE} --output ${OUTPUTS_DIR} -u https://ckan.tacc.utexas.edu/lidar_files/
+potreeconverter -i ${converterInput} -o ${OUTPUTS_DIR}
+echo ${_tapisDtnSystemOutputDir}
+echo ${_tapisDtnSystemInputDir}
