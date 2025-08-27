3921581560d8aa772673e914ee7e9acb38839cc7
Maximiliano Osorio <maxiosorio@gmail.com>
2025-08-27 10:07:09 -0400

    fix: avoid copy

--- run.sh
+++ run.sh
@@ -13,20 +13,20 @@ else
 	/home/potree/PotreeConverter -i ${_tapisExecSystemInputDir}/sourcelas -o ${OUTPUTS_DIR}
 fi
 
-PARENT_POINT_CLOUD_DIR=${WEB_SERVER_DIR}/${_tapisJobCreateDate}
-POINT_CLOUD_DIR=${PARENT_POINT_CLOUD_DIR}/${_tapisJobName}
+#PARENT_POINT_CLOUD_DIR=${WEB_SERVER_DIR}/${_tapisJobCreateDate}
+#POINT_CLOUD_DIR=${PARENT_POINT_CLOUD_DIR}/${_tapisJobName}
 
 # Copy the pointcloud on the webserver web-accessible
-mkdir -p ${POINT_CLOUD_DIR}
-cp -r ${OUTPUTS_DIR} ${POINT_CLOUD_DIR}
+#mkdir -p ${POINT_CLOUD_DIR}
+#cp -r ${OUTPUTS_DIR} ${POINT_CLOUD_DIR}
 
 METADATA_FILE=${OUTPUTS_DIR}/metadata.json
 METADATA_URL=${WEB_SERVER_URL}/${_tapisJobCreateDate}/${_tapisJobName}-${_tapisJobUuid}/metadata.json
-SCENE_FILE=${POINT_CLOUD_DIR}/scene.json
+SCENE_FILE=${OUTPUTS_DIR}/scene.json
 # Generate the scene file
 python potree_scene_generator.py \
 	${METADATA_FILE} \
 	--output-scene-file ${SCENE_FILE} \
 	--base-url ${METADATA_URL}
 
-setfacl -R -m u:33:rx ${PARENT_POINT_CLOUD_DIR}
+setfacl -R -m u:33:rx ${OUTPUTS_DIR}
